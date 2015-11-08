5ba1a9f99871f0a61a8515630e4c6b9091c49020
Matt Daubert <mdaubert@gmail.com>
2015-11-08 17:03:50 -0500

    Test the visibility of other players' hands

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,17 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'dealer'
 require 'rspec/collection_matchers'
+
+module Helpers
+  def card_faces(length)
+    a_string_matching(/[🂡-🃞]{#{length}}/)
+  end
+
+  def card_backs(length)
+    a_string_matching(/🂠{#{length}}/)
+  end
+end
+
+RSpec.configure do |c|
+  c.include Helpers
+end
